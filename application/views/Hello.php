e6454ca69e96eacabf7ac35c013a9d2b16c007f3
Orm Wiwitumpon <cutiemoon08@yahoo.com>
2017-11-26 02:20:05 +0700

    forgot - -

--- application/views/Hello.php
+++ application/views/Hello.php
@@ -50,7 +50,6 @@
 	</script>
 	
 	<script src="<?php echo base_url(); ?>assets/js/bootstrap.bundle.min.js"></script>
-	<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
 
 </body>
 </html>
